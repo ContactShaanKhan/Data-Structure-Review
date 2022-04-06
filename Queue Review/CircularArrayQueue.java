28026e10006a71136ac1d0462e9515e735256473
Shaan Khan <shaan.khan@stonybrook.edu>
2022-04-06 12:22:53 -0400

    Fixing circular array queue

--- Queue Review/CircularArrayQueue.java
+++ Queue Review/CircularArrayQueue.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 // Creating a queue implementation using a circular Queue
 public class CircularArrayQueue implements QueueInterface<Integer> {
     private Integer[] arr;
@@ -6,13 +8,17 @@ public class CircularArrayQueue implements QueueInterface<Integer> {
     // Holds the index of the front and rear
     private int front, rear;
 
+    private int currentSize;
+
     // Remember in a queue we add to the rear and remove from the front
 
     // Constructor just needs to intialize
     public CircularArrayQueue(int capacity) {
         this.arr = new Integer[capacity];
         this.capacity = capacity;
-        this.front = this.rear = 0;
+        this.front = 0;
+        this.rear = 0;
+        this.currentSize = 0;
     }
 
     public void enqueue(Integer obj) throws IllegalAccessException {
@@ -22,6 +28,8 @@ public class CircularArrayQueue implements QueueInterface<Integer> {
 
         arr[rear] = obj;
         rear = (rear + 1) % capacity;
+
+        this.currentSize++;
     }
 
     public Integer dequeue() throws IllegalAccessException {
@@ -30,6 +38,8 @@ public class CircularArrayQueue implements QueueInterface<Integer> {
 
         Integer out = arr[front];
         front = (front + 1) % capacity;
+
+        this.currentSize--;
 
         return out;
     }
@@ -42,11 +52,11 @@ public class CircularArrayQueue implements QueueInterface<Integer> {
     }
 
     public boolean isfull() {
-        return ((rear + 1) % capacity == front);
+        return (this.currentSize == capacity);
     }
 
     public boolean isempty() {
-        return (front == rear);
+        return (this.currentSize == 0);
     }
 
     public int getCapacity() {
@@ -58,13 +68,18 @@ public class CircularArrayQueue implements QueueInterface<Integer> {
 
         out.append("Front [");
 
-        for (int i = front; i != rear; i = (i + 1) % capacity) {
-            out.append(" (" + arr[i] + ")");
+        for (int i = this.front; i < this.front + this.currentSize; i++) {
+            out.append(" (" + arr[i % capacity] + ")");
+            
         }
 
         out.append(" ] Rear");
 
         return out.toString();
+    }
+
+    public void printActualQueue() {
+        System.out.println(Arrays.toString(arr));
     }
 
     /* ------------------------ Testing ------------------------ */
@@ -91,6 +106,27 @@ public class CircularArrayQueue implements QueueInterface<Integer> {
             queue.enqueue(7);
 
             System.out.println("State 3: " + queue);
+
+            queue.enqueue(8);
+            queue.enqueue(9);
+            queue.enqueue(10);
+            queue.enqueue(11);
+            queue.enqueue(12);
+
+            System.out.println("State 4: " + queue);
+
+            queue.dequeue();
+            queue.dequeue();
+        
+            queue.enqueue(13);
+            queue.enqueue(14);
+            queue.enqueue(15);
+
+            System.out.println("State 5: " + queue);
+            queue.printActualQueue();
+
+            System.out.println("Trying to add 1 beyond capacity...");
+            queue.enqueue(15);
         } 
         catch (IllegalAccessException e) {
             System.err.println("Invalid access to Queue");
